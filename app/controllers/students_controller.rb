1313a1af62ccd372f40b08cbf4e1f9bbbc36a323
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-10-20 20:56:23 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -22,6 +22,11 @@ class StudentsController < ApplicationController
 
   def index
     @students = Student.all
+    if params[:search]
+      @students = Student.search(params[:search])
+    else
+      @students = Student.all
+    end
   end
 
   def student_params
